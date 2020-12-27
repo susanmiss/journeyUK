107b47662260ee5bfe4507f838b1eee2cf992505
Susan <susan@humbertos-mbp.home>
2020-12-27 17:50:28 +0000

    C2 - Edit Categories add

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -63,16 +63,39 @@ class CategoriesController extends Controller
     {
         $categories = Categories::findOrFail($id);
         return view('categories.edit', compact('categories'));
+
     }
 
 
     public function update(Request $request, $id)
     {
-        $categories = Categories::findOrFail($id);
-        $categories->name = $request->name;
-        $categories->slug = $request->name;
-        $categories->save();
-        return redirect('categories');
+        // $categories = Categories::findOrFail($id);
+        // $categories->name = $request->name;
+        // $categories->slug = $request->name;
+        // $categories->save();
+        // return redirect('categories');
+
+               //dd($request);
+               $input = $request->all();
+               $categories = Categories::findOrFail($id);
+       
+               if($file = $request->file('featured_image')){
+                   if($categories->featured_image){
+                       unlink('images/featured_image/' . $categories->featured_image);
+                   }
+                   $name = uniqid() . $file->getClientOriginalName();
+                   $name = strtolower(str_replace(' ', '-', $name));
+                   $file->move('images/featured_image/', $name);
+                   $input['featured_image'] = $name;
+       
+               }
+       
+               $categories->update($input);
+               //sync with categories:
+            //    if($request->category_id){
+            //        $blog->category()->sync($request->category_id);
+            //    }
+               return redirect('categories');
     }
 
 
--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -8,11 +8,17 @@
     </div>
 
     <div class="col-md-12">
-        <form action="{{ route('categories.update', $categories->id) }}" method="post">
+        <form action="{{ route('categories.update', $categories->id) }}" method="post" enctype="multipart/form-data">
             {{method_field('patch')}}
             <div class="form-group">
                 <lable for="title">Category Name</lable>
                 <input type="text" name="name" class="form-control" value="{{ $categories->name }}">
+            </div>
+
+            <div class="form-group">
+                <label class="btn-btn-default">
+                <span class="btn btn-outline btn-sm btn-info">Featured Image </span>
+                <input type="file" name="featured_image" class="form-controller" hidden>
             </div>
       
             <button class="btn btn-primary" type="submit">
